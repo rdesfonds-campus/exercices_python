51fdb362e2dd8072b00cdb0a66d3684ad3715cee
rdesfonds-campus <romain.desfonds@gmail.com>
2026-03-30 16:08:54 +0200

    exercice 3

---
+++ exo 3.py
@@ -0,0 +1,11 @@
+# Exercice test ruff : doubler un nombre
+
+def double(nombre: int) -> int:
+
+    return nombre * 2
+
+
+# Test de la fonction en affichant le résultat
+print(double(2))   # on s'attend à 4
+print(double(10))  # on s'attend à 20
+print(double(-3))  # on s'attend à -6
